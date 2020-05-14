7335cfe62604a4cb2b00f84bd3d88b6a5d9df3f4
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2020-05-14 19:05:02 -0400

    Update

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,38 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+	item = {
+		name: '',
+		enhancement: item.enhancement,
+		durabilty: item.durabilty
+	};
+	if (item.enhancement >= 20) {
+		return 20;
+	}
+	return item.enhancement + 1;
 }
 
 function fail(item) {
-  return { ...item };
+	item = {
+		name: '',
+		enhancement: item.enhancement,
+		durabilty: item.durabilty
+	};
+	if (item.enhancement >= 15) {
+		return item.enhancement - 10;
+	}
+	return item.enhancement - 5;
 }
 
-function repair(item) {
-  return { ...item };
+function repair(item = 0) {
+	item = {
+		name: '',
+		enhancement: item.enhancement,
+		durabilty: item.durabilty
+	};
+	return (item.durabilty = 100);
 }
 
 function get(item) {
-  return { ...item };
+	return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,33 @@
 const enhancer = require('./enhancer.js');
 // test away!
+const sword = {
+	name: 'Excaliber',
+	enhancement: 20,
+	durability: 80
+};
+
+const axe = {
+	name: 'The Great Axe',
+	enhancement: 17,
+	durability: 32
+};
+describe('the repair function', () => {
+	it('should', () => {
+		expect(repair(axe)).toBe(100);
+		expect(repair(sword)).toBe(100);
+	});
+});
+
+describe('the succeed function', () => {
+	it('should', () => {
+		expect(succeed(sword)).toBe(20);
+		expect(succeed(axe)).toBe(18);
+	});
+});
+
+describe('the fail function', () => {
+	it('should', () => {
+		expect(fail(sword)).toBe(10);
+		expect(fail(axe)).toBe(7);
+	});
+});
